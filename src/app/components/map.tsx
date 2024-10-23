23b5cb0e5a77a30f1dfa42afd25e67f34a31f6bd
Payman <payman.hoseini7@gmail.com>
2024-10-23 22:18:39 +0330

    ip address check

--- src/app/components/map.tsx
+++ src/app/components/map.tsx
@@ -5,24 +5,28 @@ import 'leaflet/dist/leaflet.css'
 import { Icon } from 'leaflet'
 import { Details } from './location'
 
-export default function Map({details} : {details : Details| null | undefined}){
+export default function Map({details , error} : {details : Details| null | undefined , error : boolean}){
     const customeIcon = new Icon({
         iconUrl : "https://cdn-icons-png.flaticon.com/512/447/447031.png",
         iconSize : [38,38]
       })
     return (
-        <div className='w-[75vw] h-screen'>
-          <MapContainer center={(details?.lat != undefined && details?.lon != undefined ) ? [details?.lat, details?.lon] : [0,40]} zoom={2} scrollWheelZoom={true}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker position={(details?.lat != undefined && details?.lon != undefined ) ? [details?.lat , details?.lon] : [50,50]} icon={customeIcon}>
-              <Popup>
-                {details?.country + " / " + details?.city}
-              </Popup>
-            </Marker>
-          </MapContainer>
-        </div>
+        <>
+          {error ? <div className='text-center h-screen w-[75vw] text-5xl font-bold mt-28 animate-pulse'>IP Address Is Not Valid</div>
+          :
+          <div className='w-[75vw] h-screen'>
+            <MapContainer center={(details?.lat != undefined && details?.lon != undefined ) ? [details?.lat, details?.lon] : [0,40]} zoom={2} scrollWheelZoom={true}>
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={(details?.lat != undefined && details?.lon != undefined ) ? [details?.lat , details?.lon] : [50,50]} icon={customeIcon}>
+                <Popup>
+                  {details?.country + " / " + details?.city}
+                </Popup>
+              </Marker>
+            </MapContainer>
+          </div>}
+        </>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   const [ip , setIp] = useState("")
   let data : Details | null;
   const [details , setDetails] = useState<Details | null>()
+  const [error , setError] = useState(false)
   useEffect (()=>{
     searchIP()
   },[])
@@ -14,10 +15,46 @@ export default function Home() {
   function reciveIP(e : React.ChangeEvent<HTMLInputElement>){
     setIp(e.target.value)
   }
-  async function searchIP(){
-    data = await findLocation(ip)
-    setDetails(data)
+  function validateIpAddress(ipAddress : string){
+    const ipv4Regex = /^(\d{1,3}\.){3}\d{1,3}$/;
+    const ipv6Regex = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/;
+    let flag = false;
+    if (ipv4Regex.test(ipAddress) || ipAddress == "") {
+      const octets = ipAddress.split('.');
+      for (let i = 0; i < octets.length; i++) {
+        const octet = parseInt(octets[i], 10);
+        if (octet < 0 || octet > 255) {
+          flag = false
+        }
+      }
+      flag = true
+    }
+    else if(ipv6Regex.test(ipAddress)){
+      flag = true;
+    }
+    else if (/(?:[\w-]+\.)+[\w-]+/.test(ipAddress)){
+      flag = true
+    }
+    return flag
   }
+  async function searchIP(){
+    let validateIp = validateIpAddress(ip)
+    if(validateIp){
+      data = await findLocation(ip)
+      setDetails(data)
+      if(data.city == undefined){
+        setError(true)
+      }
+      else{
+        setError(false)
+      }
+      
+    }
+    else {
+      setError(true)
+    }
+  }
+
 
   return (
     <>
@@ -30,6 +67,8 @@ export default function Home() {
                 <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6"/></svg>
               </button>
           </div>
+          {error ?  <div className="bg-white mt-10 py-7 shadow-lg px-16 -mb-16 rounded-xl z-i">Error</div>
+          :
           <div className="mt-10 flex bg-white py-7 px-6 rounded-xl -mb-16 shadow-lg w-[70%] z-i ">
               <div className="pr-10 border-r border-r-gray-300">
                 <p className="text-Dark-Gray text-[10px] font-bold tracking-widest">IP ADDRESS</p>
@@ -47,9 +86,9 @@ export default function Home() {
                 <p className="text-Dark-Gray text-[10px] font-bold tracking-widest">ISP</p>
                 <p className="text-Very-Dark-Gray font-medium mt-1 text-2xl">{details?.isp}</p>
               </div>
-          </div>
+          </div>}
         </div>
-        <Map details={details}/>
+        <Map details={details} error={error}/>
       </main>
     </>
   );
