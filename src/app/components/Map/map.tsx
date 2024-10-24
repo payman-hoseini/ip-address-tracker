68e54e089e81ef1a25775e0b961bda901cd6a841
Payman <payman.hoseini7@gmail.com>
2024-10-24 14:55:41 +0330

    disable server side rendering for map because react-leaflet dosent support this

--- src/app/components/Map/map.tsx
+++ src/app/components/Map/map.tsx
@@ -1,15 +1,17 @@
 'use client'
-import { MapContainer, Marker, Popup } from 'react-leaflet'
-import { TileLayer } from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-import { Icon } from 'leaflet'
-import { Details } from './location'
+// import { MapContainer, Marker, Popup } from 'react-leaflet'
+// import { TileLayer } from 'react-leaflet'
+// import { Icon } from 'leaflet'
+import { Details } from '../location'
 
 export default function Map({details , error} : {details : Details| null | undefined , error : boolean}){
+    const {Icon} = require('leaflet')
+    const {MapContainer, Marker, Popup , TileLayer} = require('react-leaflet');
     const customeIcon = new Icon({
         iconUrl : "https://cdn-icons-png.flaticon.com/512/447/447031.png",
         iconSize : [38,38]
       })
+     
     return (
         <>
           {error ? <div className='text-center h-screen lg:w-[75vw] text-3xl md:text-5xl font-bold mt-28 animate-pulse'>IP Address Is Not Valid</div>
